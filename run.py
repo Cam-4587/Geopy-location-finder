a3a6d6aab8461446c4fbbcb6885510381665e60b
Cam-4587 Conroy <cameronconroy@live.com>
2025-05-13 17:01:41 +0000

    add additional files for deployment, refactor code to separate into individual functions, and add option to clear terminal

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from geopy.geocoders import Nominatim
+from replit import clear
 
-def coordinates_finder():
-    geolocator = Nominatim(user_agent="my_user_agent")
+geolocator = Nominatim(user_agent="my_user_agent")
+
+def city_and_country_input():
     city = input("Enter City: ")
     while not city:
         print("Please enter a city")
@@ -9,17 +11,33 @@ def coordinates_finder():
     country = input("Enter Country: ")
     while not country:
         print("Please enter a country: ")
-        country = input("Enter Country: ")
-    if ValueError:
-        print("Invalid Coordinates")
-        city = input("Enter City: ")
-        while not city:
-            print("Please enter a city")
-            city = input("Enter City: ")
-        country = input("Enter Country: ")
-        while not country:
-            print("Please enter a country: ")
-            country = input("Enter Country: ")
-    loc = geolocator.geocode(city+','+ country)
-    print("latitude is :-" ,loc.latitude,"\nlongtitude is:-" ,loc.longitude)
-coordinates_finder()
+        country = input("Enter Country: ")    
+    coordinates_finder(city, country)
+
+def coordinates_finder(city, country):
+        loc = geolocator.geocode(city+','+ country)
+        if loc is None:
+            print("Unable to find your location try again")
+            city_and_country_input()
+        else:
+            print("\nlatitude is :- " ,loc.latitude, "|" " longtitude is:-" ,loc.longitude)
+            continue_or_not()
+
+def continue_or_not():
+    clear_console = input(
+            "\nWould you like to clear the console? (yes/no): "
+        ).lower()
+    if clear_console in ["yes", "y"]:
+        clear()
+        city_and_country_input()
+    if clear_console in ["no", "n"]:
+        print("\n")
+        city_and_country_input()
+    while clear_console not in ["yes", "no"]:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        clear_console = input(
+            "Would you like to clear the console? (yes/no): "
+        ).lower()
+
+
+city_and_country_input()
